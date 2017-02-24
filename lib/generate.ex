4614720d9f4558a475b55307cd48e2d6aeb8da1f
Sean Clayton 🎷🐢 <sclayton@swifttrip.com>
2017-02-24 13:10:07 -0500

    Add caching

--- lib/generate.ex
+++ lib/generate.ex
@@ -7,6 +7,7 @@ defmodule Gravi.Generate do
     string = Enum.join(conn.params["glob"], "/")
     conn
     |> put_resp_content_type("image/svg+xml")
+    |> put_resp_header("cache-control", "public, max-age=86400")
     |> send_resp(200, Graditar.generate(string))
   end
 end
